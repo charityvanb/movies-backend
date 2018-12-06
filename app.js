8182fe9767be3c322d19fbecffd0cd5b9277cf88
charityvanb <charityvanb@gmail.com>
2018-12-06 13:28:51 -0700

    working on movies file

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require("express")
+const app = express()
+const bodyParser = require("body-parser")
+const cors = require("cors")
+const port = process.env.PORT || 3004
+const movies = require('./routes/movies')
+
+app.use(cors())
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(('/movies', movies))
+
+app.listen(port, () => {
+    console.log(`running on port: ${port}`)
+})
