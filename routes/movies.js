b892ce25e56ff4c4ccb5490305e6089ec63dbaa7
charityvanb <charityvanb@gmail.com>
2018-12-06 13:45:33 -0700

    got movies.j working

--- routes/movies.js
+++ routes/movies.js
@@ -2,7 +2,7 @@ var express = require('express')
 var router = express.Router()
 
 router.get('/', (req,res) => {
-    releaseEvents.status(200).send('you got the thing')
+    res.status(200).send('you got the thing')
 })
 
 module.exports = router
